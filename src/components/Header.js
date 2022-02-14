ba06452978738e7eed804a4e7bd9e3c3267d95e9
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-02-14 13:52:17 +0100

    nav first element created

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,53 @@ import React from 'react'
 import styled from 'styled-components'
 
 function Header() {
-  return (
+    return (
+        
     <Nav>
-        <Logo src="" />
-        <NavMenu>
-
-
-
-        </NavMenu>
-
-        </Nav>
-  )
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+      </NavMenu>
+      </Nav>
+    )
 }
 
 export default Header
 
+
+
 const Nav = styled.nav`
     height: 70px;
     background: #090b13;
-`
+    display: flex;
+    align-item: center;
+    padding: 0 36px;
+    `
 
 const Logo = styled.img`
-    width:80px;
-`
+    width: 80px;
+    `
 
 const NavMenu = styled.div`
+  display: flex;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
 
 `
+
