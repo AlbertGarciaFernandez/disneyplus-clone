4f48fc0442ae18557c4fcefbded0949989e75e58
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-02-14 18:54:30 +0100

    routering add

---
+++ src/components/Detail.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Detail() {
+  return (
+    <Container>
+        Detail
+    </Container>
+  )
+}
+
+export default Detail
+
+const Container = styled.div`
+    min-height: calc (100vh -70px);
+    padding: 0 calc(3.5vw + 5px);
+`
+const Background = styled.div``
